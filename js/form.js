6e543e99bf1cb997ddf9aeb86ef30f958ac82e2c
Rodrigo Chigua <88567618+CazadorespecialANBU@users.noreply.github.com>
2022-02-25 01:53:45 -0400

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,12 @@
+document.querySelector('#submit').addEventListener('click',function(){
+    
+    let cliente = document.querySelector('#cliente').value;
+    let fecha = document.querySelector('#fecha').value;
+    let hora = document.querySelector('#hora').value;
+    let hacker = document.querySelector('#hacker').value;
+    let servicio = document.querySelector('#servicio').value;
+
+    let url = "https://api.whatsapp.com/send?phone=59175816973&text=*_Por fin :)_*%0A*¿ Que actividad te gustaria hacer ?*%0A%0A*¿Escribe tu nombre?*%0A" + cliente + "%0A*¿ Que dia tienes tiempo ?*%0A" + fecha + "%0A*¿ Que hora tienes tiempo ?*%0A" + hora + "%0A*¿ Que te gustaria comer ?*%0A" + hacker + "%0A*¿Donde te gustaria ir?*%0A" + servicio;
+    window.open(url);
+
+});
