3ef50432eae105520648f10109faa6b10caa17e7
Rodrigo Chigua <88567618+c4z4dor@users.noreply.github.com>
2022-04-24 00:59:07 -0400

    Update form.js

--- js/form.js
+++ js/form.js
@@ -6,7 +6,7 @@ document.querySelector('#submit').addEventListener('click',function(){
     let hacker = document.querySelector('#hacker').value;
     let servicio = document.querySelector('#servicio').value;
 
-    let url = "https://api.whatsapp.com/send?phone=59175816973&text=*_Por fin :)_*%0A*¿ Que actividad te gustaria hacer ?*%0A%0A*¿Escribe tu nombre?*%0A" + cliente + "%0A*¿ Que dia tienes tiempo ?*%0A" + fecha + "%0A*¿ Que hora tienes tiempo ?*%0A" + hora + "%0A*¿ Lugar ?*%0A" + hacker + "%0A*¿Donde te gustaria ir?*%0A" + servicio;
+    let url = "https://api.whatsapp.com/send?phone=59177205243&text=*_Por fin :)_*%0A*¿ Que actividad te gustaria hacer ?*%0A%0A*¿Escribe tu nombre?*%0A" + cliente + "%0A*¿ Que dia tienes tiempo ?*%0A" + fecha + "%0A*¿ Que hora tienes tiempo ?*%0A" + hora + "%0A*¿ Lugar ?*%0A" + hacker + "%0A*¿Donde te gustaria ir?*%0A" + servicio;
     window.open(url);
 
 });
